0b451c3173843f46acd3dc953d82cd70f3ff2252
nkh1006 <mqsora@naver.com>
2024-01-30 11:37:02 +0900

    route

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 import express from "express";
 
+const express = require("express");
 const app = express();
+const router = express.Router();
 
-app.get("/", (req, res) => console.log("hello"));
+router.use((req, res, next) => {
+  console.log("Time:", Date.now());
+  next(); // 다음 콜백 함수로 제어를 넘긴다.
+});
+
+router.get("/hello", (req, res) => {
+  res.send("hello world");
+});
+
+// 경로가 /dev로 시작하는 요청에 대해 router 마운트
+app.use("/dev", router);
 
 app.listen(3000, () => console.log("Express app listening on port 3000"));
