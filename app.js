43458938cade0dfa9539be8d41e270d43c384418
nkh1006 <mqsora@naver.com>
2024-02-02 00:25:34 +0900

    route update

--- app.js
+++ app.js
@@ -5,18 +5,35 @@ const path = require("path");
 const app = express();
 const router = express.Router();
 
-app.use("/static", express.static(path.join(__dirname, "public")));
-
 router.use((req, res, next) => {
   console.log("Time:", Date.now());
   next(); // 다음 콜백 함수로 제어를 넘긴다.
 });
 
+// 라우트 1 (콜백 함수 1 + 콜백 함수 2)
+router.get("/hello", [
+  (req, res) => {
+    console.log("hello world 1");
+    next("route"); // 두 번째 콜백 함수를 건너뛴다.
+  },
+  (req, res) => {
+    console.log("hello world 2"); // 실행되지 않는다.
+  },
+]);
+
+// 라우트 2 (콜백 함수 3)
 router.get("/hello", (req, res) => {
-  res.send("hello world");
+  console.log("hello world 3");
+  next("router"); // 네 번째 콜백 함수를 건너뛴다.
 });
 
-// 경로가 /dev로 시작하는 요청에 대해 router 마운트
+// 라우트 3 (콜백 함수 4)
+router.get("/hello", (req, res) => {
+  console.log("hello world 4"); // 실행되지 않는다.
+});
+
 app.use("/dev", router);
 
-app.listen(3000, () => console.log("Express app listening on port 3000"));
+app.get("/dev/hello", (req, res) => {
+  res.send("end");
+});
